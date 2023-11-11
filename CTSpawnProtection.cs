6200b2acc112a1b227b64c57b0f6f838dab8b534
hackmastr <win1dows8@ymail.com>
2023-11-11 19:54:34 +0100

    Initial push

---
+++ CTSpawnProtection.cs
@@ -0,0 +1,102 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Timers;
+using Timer = CounterStrikeSharp.API.Modules.Timers.Timer;
+
+namespace CTSpawnProtection;
+
+public class CTSpawnProtection : BasePlugin
+{
+    public override string ModuleName => "[CT] Spawn protection.";
+    public override string ModuleAuthor => "livevilog";
+    public override string ModuleVersion => "0.2.0";
+
+    private const int PreventiveHealth = 100; // Cuanta vida necesitamos para mantener el jugador vivo
+    private const int ProtectionDuration = 15 + 1; // Duracion protection spawn en segundos + 1
+    
+    private readonly Dictionary<CCSPlayerController, bool> _protectedList = new ();
+    private readonly Dictionary<CCSPlayerController, DateTime> _spawnTimings = new ();
+    
+    private Timer? _clearProtectionTimer;
+
+    public override void Load(bool hotReload)
+    {
+        if (hotReload)
+        {
+            InitializeLists();
+        }
+        
+        _clearProtectionTimer = new Timer(.5f, ClearProtectionTimer, TimerFlags.REPEAT);
+        Timers.Add(_clearProtectionTimer);
+    }
+
+    private void InitializeLists()
+    {
+        Utilities.GetPlayers().ForEach(controller =>
+        {
+            _protectedList[controller] = false;
+            _spawnTimings[controller] = DateTime.Now;
+        });
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        _clearProtectionTimer!.Kill();
+        Timers.Remove(_clearProtectionTimer);
+        _clearProtectionTimer = null;
+        
+        _spawnTimings.Clear();
+        _protectedList.Clear();
+    }
+
+    private void ClearProtectionTimer()
+    {
+        Utilities.GetPlayers().ForEach(controller =>
+        {
+            if (!_spawnTimings.TryGetValue(controller, out var time)) 
+                return;
+            
+            if ((DateTime.Now - time).Seconds > ProtectionDuration && _protectedList[controller])
+            {
+                _protectedList[controller] = false;
+                controller.PrintToChat("You are no longer spawn protected.");
+            }
+        });
+    }
+
+    [GameEventHandler(HookMode.Post)]
+    public HookResult OnPlayerSpawn(EventPlayerSpawn @event, GameEventInfo _)
+    {
+        _protectedList[@event.Userid] = true;
+        _spawnTimings[@event.Userid] = DateTime.Now;
+        
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler(HookMode.Post)]
+    public HookResult OnPlayerHurt(EventPlayerHurt @event, GameEventInfo _)
+    {
+        if (!_protectedList[@event.Userid]) return HookResult.Continue;
+        
+        @event.Userid.PlayerPawn.Value.Health = PreventiveHealth;
+        
+        return HookResult.Changed;
+    }
+
+    [GameEventHandler(HookMode.Post)]
+    public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo _)
+    {
+        _protectedList.Remove(@event.Userid);
+        
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler(HookMode.Post)]
+    public HookResult OnPlayerConnectFull(EventPlayerDisconnect @event, GameEventInfo _)
+    {
+        _protectedList[@event.Userid] = false;
+        
+        return HookResult.Continue;
+    }
+}
